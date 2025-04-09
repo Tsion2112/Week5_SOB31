1c23d2f5859830124e89e79bad2b925e724c1f0a
Tsion2112 <TY169@live.mdx.ac.uk>
2025-04-09 22:08:29 +0400

    Update broken_pirate.py

--- broken_pirate.py
+++ broken_pirate.py
@@ -1,5 +1,10 @@
-greeting = input("Hello, possible pirate! What's the password?")#Tsion added "
-if greeting in ["Arrr!"):
-	print("Go away, pirate.")
-elif
-print("Greetings, hater of pirates!")
+# Tsion fixed: added proper parentheses and colons, fixed logic issues
+greeting = input("Hello, possible pirate! What's the password?")  # Tsion added input for pirate password
+
+# Tsion fixed: mismatched parentheses and incomplete condition
+if greeting in ["Arrr!"]:  # Tsion changed the incorrect list syntax (missed closing parenthesis)
+    print("Go away, pirate.")  # Tsion added this line to print if password matches "Arrr!"
+
+# Tsion fixed: 'elif' was incomplete, added correct condition and print statement
+else:  # Tsion added 'else' to handle the case when the password is not "Arrr!"
+    print("Greetings, hater of pirates!")  # Tsion added this line to greet non-pirates
